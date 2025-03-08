986c1c0415503a4418c3d6003796dd92bb7a6611
Anna <134068104+Annechka12@users.noreply.github.com>
2025-03-08 15:50:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+﻿document.getElementById("bookingForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Вы успешно записаны! Я свяжусь с вами для подтверждения.");
+});
